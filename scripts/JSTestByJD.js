7d6de022fc4bb3b8e83c023fde15b4aa1ed1cb53
KimJung-Do <kimjung-do@KimJung-Dos-MacBook-Air.local>
2018-11-04 13:32:37 +0900

    adding my files to my repository

---
+++ scripts/JSTestByJD.js
@@ -0,0 +1,42 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = "Hello World!";
+
+function multiply(num1,num2){
+    var result = num1*num2;
+    return result;
+}
+
+var myHTML = document.querySelector('html')
+// myHTML.onclick = function() {
+//     alert(multiply(4,5));
+// }
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/WebTestImage_01.png'){
+        myImage.setAttribute('src', 'images/WebTestImage_02.png');
+    }else{
+        myImage.setAttribute('src', 'images/WebTestImage_01.png');
+    }
+}
+
+var myButton = document.querySelector('button')
+var myHeading = document.querySelector('h1')
+
+function setUserName(){ // 타이틀의 이름 바꾸기 함수. 로컬스토리지를 이용!
+    var myName = prompt('Please enter your name.'); // 대화상자를 불러오는 함수. OK 버튼을 누르면 값을 Local에 저장.
+    localStorage.setItem('name',myName);    // name이라는 항목을 생성 후 저장
+    myHeading.innerHTML = 'Mozilla is cool, '+myName;   
+    if(!localStorage.getItem('name')){
+        setUserName();
+    }else{
+        var storageName = localStorage.getItem('name');
+        myHeading.innerHTML = 'Mozilla is cool, '+storageName;
+    }
+}
+
+myButton.onclick = function(){
+    setUserName();
+}
